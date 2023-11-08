ce7e20f0346e4867ec047ab03a132bd7b1ac2b57
Iain Emslie <iainemslie@gmail.com>
2023-11-08 09:50:50 -0500

    scout health

---
+++ 35_scout_health.md
@@ -0,0 +1,9 @@
+# Scout Health
+
+### The scout has a hit function
+
+### But no health, so being hit doesn't matter
+
+### We'll give him health and reduce it when he is hit
+
+### We will also need a timer
--- Shooter/scenes/enemies/scout.gd
+++ Shooter/scenes/enemies/scout.gd
@@ -4,10 +4,18 @@ var player_nearby: bool = false
 var can_laser: bool = true
 var first_gun: bool = true
 
+var health: int = 30
+var vulnerable: bool = true
+
 signal laser(pos, direction)
 
 func hit():
-	print("scout was hit")
+	if vulnerable:
+		health -= 10
+		vulnerable = false
+		$Timers/HitTimer.start()
+	if health <= 0:
+		queue_free()
 
 func _process(_delta):
 	if player_nearby:
@@ -25,15 +33,16 @@ func _process(_delta):
 				first_gun = true
 
 			can_laser = false
-			$LaserCooldown.start()
+			$Timers/LaserTimer.start()
 
 func _on_attack_area_body_entered(_body):
 	player_nearby = true
 
-
 func _on_attack_area_body_exited(body):
 	player_nearby = false	
 
-
-func _on_laser_cooldown_timeout():
+func _on_laser_timer_timeout():
 	can_laser = true
+
+func _on_hit_timer_timeout():
+	vulnerable = true
