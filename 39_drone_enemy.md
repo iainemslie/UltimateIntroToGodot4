665bca48adfe35f25f7682750c9b5dbff24ef799
Iain Emslie <iainemslie@gmail.com>
2023-11-09 13:54:28 -0500

    Making the drone explode

---
+++ 39_drone_enemy.md
@@ -0,0 +1,5 @@
+# Updating the drone
+
+### Once the drone spots the player it moves towards the player and increased the speed
+
+### Once it collides with anything it explodes
--- Shooter/scenes/enemies/drone.gd
+++ Shooter/scenes/enemies/drone.gd
@@ -1,14 +1,54 @@
 extends CharacterBody2D
 
-func _process(_delta):
-	# direction
-	var direction = Vector2.RIGHT
-	
-	# velocity
-	velocity = direction * 200
-	
-	# move and slide
-	move_and_slide()
+var active: bool = false
+var max_speed: int = 600
+var speed: int = 0
+var speed_multiplier: int = 1
+
+var vulnerable: bool = true
+var health: int = 50
+
+var explosion_active: bool = false
+
+func _ready():
+	$Explosion.hide()
+	$Sprite2D.show()
+
+func _process(delta):
+	if active:
+		look_at(Globals.player_pos)
+		var direction = (Globals.player_pos - position).normalized()
+		velocity = direction * speed * speed_multiplier
+		var collision = move_and_collide(velocity * delta)
+		if collision:
+			$AnimationPlayer.play("explosion")
+			explosion_active = true
+	if explosion_active:
+		var targets = get_tree().get_nodes_in_group("Container") + get_tree().get_nodes_in_group("Entity")
+		for target in targets:
+			var in_range = target.global_position.distance_to(global_position) < 400
+			if "hit" in target and in_range:
+				target.hit()
 
 func hit():
-	print('damage')
+	if vulnerable:
+		health -= 10
+		vulnerable = false
+		$HitTimer.start()
+		$Sprite2D.material.set_shader_parameter("progress", 0.4)
+	if health <= 0:
+		$AnimationPlayer.play("explosion")
+		explosion_active = true
+			
+func stop_movement():
+	speed_multiplier = 0
+
+func _on_notice_area_body_entered(_body):
+	active = true
+	var tween = create_tween()
+	tween.tween_property(self, "speed", max_speed, 6)
+
+func _on_hit_timer_timeout():
+	vulnerable = true
+	$Sprite2D.material.set_shader_parameter("progress", 0)
+	
